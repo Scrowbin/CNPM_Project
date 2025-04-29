11d5bff0457e52acc70969833c2805ed1ea8500a
Scrowbin <kongudau2@gmail.com>
2025-04-29 19:17:59 +0700

    frmrental designererer

---
+++ GUI/frmRental.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace GUI
+{
+    partial class frmRental
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button5 = new Button();
+            button1 = new Button();
+            button2 = new Button();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            panel2 = new Panel();
+            pictureBox1 = new PictureBox();
+            panel1 = new Panel();
+            panel3 = new Panel();
+            label1 = new Label();
+            flowLayoutPanel1.SuspendLayout();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel3.SuspendLayout();
+            SuspendLayout();
+            // 
+            // button5
+            // 
+            button5.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            button5.AutoEllipsis = true;
+            button5.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button5.Location = new Point(3, 156);
+            button5.Name = "button5";
+            button5.Size = new Size(165, 40);
+            button5.TabIndex = 17;
+            button5.Text = "Ngắn hạn";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button1
+            // 
+            button1.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            button1.AutoEllipsis = true;
+            button1.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.Location = new Point(3, 202);
+            button1.Name = "button1";
+            button1.Size = new Size(165, 40);
+            button1.TabIndex = 18;
+            button1.Text = "Du lịch";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            button2.AutoEllipsis = true;
+            button2.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button2.Location = new Point(3, 248);
+            button2.Name = "button2";
+            button2.Size = new Size(165, 40);
+            button2.TabIndex = 19;
+            button2.Text = "Dài hạn";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.BackColor = SystemColors.ActiveCaption;
+            flowLayoutPanel1.Controls.Add(panel2);
+            flowLayoutPanel1.Controls.Add(button5);
+            flowLayoutPanel1.Controls.Add(button1);
+            flowLayoutPanel1.Controls.Add(button2);
+            flowLayoutPanel1.Dock = DockStyle.Left;
+            flowLayoutPanel1.FlowDirection = FlowDirection.TopDown;
+            flowLayoutPanel1.Location = new Point(0, 0);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(168, 664);
+            flowLayoutPanel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = SystemColors.ActiveCaption;
+            panel2.Controls.Add(pictureBox1);
+            panel2.Location = new Point(3, 3);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(165, 147);
+            panel2.TabIndex = 24;
+            panel2.Paint += panel2_Paint;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Anchor = AnchorStyles.None;
+            pictureBox1.Cursor = Cursors.No;
+            pictureBox1.Image = Properties.Resources.Home_owner;
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.MaximumSize = new Size(160, 160);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(160, 144);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 15;
+            pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.Highlight;
+            panel1.Location = new Point(168, 47);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(828, 617);
+            panel1.TabIndex = 21;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = SystemColors.Highlight;
+            panel3.Controls.Add(label1);
+            panel3.Dock = DockStyle.Top;
+            panel3.Location = new Point(168, 0);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(828, 49);
+            panel3.TabIndex = 22;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Tahoma", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.ButtonFace;
+            label1.Location = new Point(247, 3);
+            label1.Name = "label1";
+            label1.Size = new Size(295, 39);
+            label1.TabIndex = 0;
+            label1.Text = "CĂN HỘ NGẮN HẠN";
+            // 
+            // frmRental
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(996, 664);
+            Controls.Add(panel3);
+            Controls.Add(panel1);
+            Controls.Add(flowLayoutPanel1);
+            Name = "frmRental";
+            Text = "Form1";
+            flowLayoutPanel1.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private Button button2;
+        private Button button1;
+        private Button button5;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Panel panel1;
+        private Panel panel2;
+        private PictureBox pictureBox1;
+        private Panel panel3;
+        private Label label1;
+    }
+}
